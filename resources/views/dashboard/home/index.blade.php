d9bc59aa15c42d6a3b70f559cc4cac3b1166cd15
matiuskm <matius.km@gmail.com>
2025-01-17 22:27:20 +0700

    Add dashboard route, controller, and views

---
+++ resources/views/dashboard/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dashboard.dashboard-layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Dashboard</h1>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\Site\HomeController::class, 'index'])->name('site.home');
 Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'index'])->name('auth.login');
+Route::get('/dashboard', [App\Http\Controllers\Dashboard\HomeController::class, 'index'])->name('dashboard.home');
