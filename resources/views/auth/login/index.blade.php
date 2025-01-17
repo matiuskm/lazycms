640af90c608a2c4295a92fbd77bbde9ea2a4811a
matiuskm <matius.km@gmail.com>
2025-01-17 22:58:29 +0700

    Implement authentication layout with login form and footer

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.auth.auth-layout')
+@section('title', 'Login')
 
 @section('content')
-    Login Page
+<div class="container">
+    <div class="row justify-content-center">
+       <div class="col-lg-5">
+          <div class="card shadow-lg border-0 rounded-lg mt-5">
+             <div class="card-header">
+                <h3 class="text-center font-weight-light my-4">Login</h3>
+             </div>
+             <div class="card-body">
+                <form action="" method="POST">
+                   <div class="form-floating mb-3">
+                      <input name="email" class="form-control" id="inputEmail" type="email" placeholder="Enter email..."
+                         autocomplete="username">
+                      <label for="inputEmail">Email</label>
+                   </div>
+                   <div class="form-floating mb-3">
+                      <input name="password" class="form-control" id="inputPassword" type="password" placeholder="Enter password..."
+                         autocomplete="current-password">
+                      <label for="inputPassword">Password</label>
+                   </div>
+                   <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                      <span>
+                         <a class="small" href="/">Home</a> /
+                         <a class="small" href="/">Forgot Password?</a>
+                      </span>
+                      <button type="submit" class="btn btn-primary">Login</button>
+                   </div>
+                </form>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
 @endsection
--- resources/views/layouts/auth/auth-layout.blade.php
+++ resources/views/layouts/auth/auth-layout.blade.php
@@ -1,13 +1,29 @@
+@php
+    $app = [
+        'name' => config('app.name'),
+        'year' => date('Y')
+    ]
+@endphp
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Auth Layout</title>
+    <title>{{ $app['name'] }} | @yield('title')</title>
+    @vite('resources/sass/app.scss')
 </head>
-<body>
-    @yield('content')
-</body>
+<body class="bg-dark">
+    <div id="layoutAuthentication">
+       <div id="layoutAuthentication_content">
+          <main>
+             @yield('content')
+          </main>
+       </div>
+       @include('layouts.auth._footer')
+    </div>
+
+    @vite('resources/js/app.js')
+ </body>
 </html>
 
